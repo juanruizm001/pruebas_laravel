e6f644c99a75e60e3a0fa40a9974ebfb74c6986d
Juan Ruiz <juanruizm001@gmail.com>
2015-05-24 14:07:18 -0300

    creacion de array de grupos para validar sesiones

--- app/routes.php
+++ app/routes.php
@@ -19,16 +19,21 @@ Route::get('/', function()
 Route::pattern('nombre', '[a-z, A-Z]+');
 Route::pattern('edad', '[0-9]+');
 
-
-Route::get('profesores/{nombre}/{edad}', function($nombre, $edad)
+//Validacion de grupos mediante sesiones
+Route::group(array('before'=>'session'), function()
 {
-    return "Hola $nombre tu edad es $edad";
-})/*->where(array('nombre'=>'[a-z, A-Z]+', 'edad'=>'[0-9]+'))*/;
+    Route::get('profesores/{nombre}/{edad}', function($nombre, $edad)
+    {
+        return "Hola $nombre tu edad es $edad";
+    })/*->where(array('nombre'=>'[a-z, A-Z]+', 'edad'=>'[0-9]+'))*/;
 
-Route::get('estudiantes/{nombre}/{edad}', array('before'=>'session', function($nombre, $edad)
-{
-    return "Hola $nombre tu edad es $edad";
-}));
+    Route::get('estudiantes/{nombre}/{edad}', function($nombre, $edad)
+    {
+        return "Hola $nombre tu edad es $edad";
+    });
+});
+
+
 
 Route::get('session/crear', function()
 {
