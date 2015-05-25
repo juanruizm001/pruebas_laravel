4427c0593e968eae2fae1f6a6935b3ba76587b68
Juan Ruiz <juanruizm001@gmail.com>
2015-05-25 18:04:41 -0300

    integracion con twitter boostrap (vista)

--- app/views/hola.blade.php
+++ app/views/hola.blade.php
@@ -7,9 +7,35 @@
 @section('content')
     <h1>Bienvenidos</h1>
     <p>Este el el contenido de la vista</p>
+
+
 @stop
 
 @section('footer')
     @parent
     <p>Este es el pie de página de la vista</p>
 @stop
+
+{{--
+Trabajo con variables
+{{ $nombrevariable }}
+{{{ $nombrevariable }}} <!-- Escapa los caracteres para prevenir injeccion sql -->
+@{{ $nombrevariable }} <!-- Escapa los caracteres para prevenir injeccion sql -->
+{{ time() }}
+{{{ $name or 'Valor por defecto' }}}
+{{{ isset($name) ? $name : 'Valor por defecto' }}}
+
+@foreach($a as $b)
+    ...
+@endforeach
+
+@if($a == 1)
+    ...
+@else
+    ...
+@endif
+
+@unless(Auth::check())
+    Usted no está logeado.
+@endunless
+--}}
